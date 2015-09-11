1d6919d70a7b2322a432ede41973e72fd16fd2c3
Mark Reeves <mjr@clearbold.com>
2015-09-11 14:22:41 -0400

    Added preventDefault() to MENU link

--- ui/js/scripts.js
+++ ui/js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $('.show-mobile-nav').on('click',function(e){
+        e.preventDefault();
         $('.main-nav ul').toggleClass('show');
     })
     $('.fancybox').fancybox({
